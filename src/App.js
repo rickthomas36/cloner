ad215d7490ed40ca522197f72a54d756b70d1972
rickthomas36 <109500699+rickthomas36@users.noreply.github.com>
2024-12-20 21:56:51 -0500

    Netflix_Clone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Clever Programmers, Let's Build a React App!!</h1>
+    </div>
+  );
+}
+
+export default App;
+
